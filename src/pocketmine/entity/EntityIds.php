c031122a772545d774905045e1f1ab5cefae002f
Irsan Agil <53559769+xSoapers@users.noreply.github.com>
2020-03-14 12:02:07 +0700

    Added bee and ravager

--- src/pocketmine/entity/EntityIds.php
+++ src/pocketmine/entity/EntityIds.php
@@ -127,4 +127,6 @@ interface EntityIds{
 	public const TROPICALFISH = 111, TROPICAL_FISH = 111;
 	public const COD = 112, FISH = 112;
 	public const PANDA = 113;
+	public const BEE = 114;
+	public const RAVAGER = 115;
 }

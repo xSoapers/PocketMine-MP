9113c30c302a13aef976f56dedf78b60d34356f3
Irsan Agil <53559769+xSoapers@users.noreply.github.com>
2020-03-14 15:32:20 +0700

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -32,7 +32,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 }
 const _VERSION_INFO_INCLUDED = true;
 
-const NAME = "PocketMine-MP";
+const NAME = "Minecrush-MP";
 const BASE_VERSION = "3.11.7";
-const IS_DEVELOPMENT_BUILD = true;
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
